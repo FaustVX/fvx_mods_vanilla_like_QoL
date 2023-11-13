8120c50dc07d22297c498191226533300ddb2c9a
FaustVX <jodu035@gmail.com>
2023-11-13 19:47:13 +0100

    Check version in `changelog.txt`

--- bump_version.ps1
+++ bump_version.ps1
@@ -6,6 +6,14 @@ param (
     [String]$message
 )
 
+if (-not (Get-Content .\mod\changelog.txt)[1].Contains($version))
+{
+    echo "Invalid version in ``changelog.txt``"
+    exit 1
+}
+
+$version = "v$version"
+
 if ($PSBoundParameters.ContainsKey('message'))
 {
     git commit -am "Bumped version ``$version```n$message"
@@ -14,6 +22,7 @@ else
 {
     git commit -am "Bumped version ``$version``"
 }
-git push
+
 git tag $version
+git push
 git push --tags
--- bump_version.sh
+++ bump_version.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
+line=$(head -n "+2" "mod/changelog.txt" | tail -1)
+version=$1
+message=$2
+
+if [[ "$line" != *"$version"* ]]; then
+    echo "Invalid version in ``changelog.txt``"
+    exit 1
+fi
+version="v$version"
+
 if [ $# -eq 1 ]; then
-    git commit -am "Bumped version \`$1\`"
+    git commit -am "Bumped version \`$version\`"
 elif [ $# -eq 2 ]; then
-    git commit -am "Bumped version \`$1\`\n$2"
+    git commit -am "Bumped version \`$version\`\n$message"
 else
     echo "Exactly 1 or 2 argument must be provided"
     exit false
 fi
-git commit -am "Bumped version \`$1\`"
+
+git tag "$version"
 git push
-git tag "$1"
 git push --tags
